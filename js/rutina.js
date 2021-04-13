31d3b571256b271cd8e9e129a20cd18684ffa043
Rayhsvr <rayhsvr@gmail.com>
2021-04-12 21:13:56 -0500

    Versión con conversión de String a Numero

--- js/rutina.js
+++ js/rutina.js
@@ -1,4 +1,9 @@
-var nombre = prompt("Proporciona tu nombre")
-var edad = prompt("Proporciona tu edad")
+let nombre = prompt("Proporciona tu nombre")
+let articulo1 = prompt("Proporciona precio primer articulo")
+let articulo2 = prompt("Proporciona precio segundo articulo")
+let suma = 0
+suma = parseInt(articulo1)+parseInt(articulo2)
 document.write('<h1>Bienvenido(a) ' + nombre + '</h1>')
-document.write('<p>tu edad es ' + edad + '</p>')
+document.write('<p>Articulo 1 $' + articulo1 + '</p>')
+document.write('<p>Articulo 2 $' + articulo2 + '</p>')
+document.write('<p>El importe = $' + suma + '</p>')
